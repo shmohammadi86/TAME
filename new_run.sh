4c527d2bdf1faa7a1bb71779426cb15da5ea758e
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2016-02-28 13:14:10 -0500

    First syntatically correct version with triangle scores replaced with Delta structure

--- new_run.sh
+++ new_run.sh
@@ -1,2 +1,2 @@
-./tri-match -t smat -H input/BioGRID/BioGRID_full_yeast_net.smat -G input/BioGRID/BioGRID_full_human_net.smat -S input/BioGRID/BioGRID_full_human-yeast.smat -Y 2 -b 1 -a 10 --iter 2 -x seqsim
+./tri-match -t smat -H input/BioGRID/BioGRID_full_yeast_net.smat -G input/BioGRID/BioGRID_full_human_net.smat -S input/BioGRID/BioGRID_full_human-yeast.smat -Y 2 -b 1 -a 1 --iter 3 -x seqsim
 
