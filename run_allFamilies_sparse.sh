4f59a26602848352a566d6e7ed8ffdf5ae1e71f3
Shahin Mohammadi <mohammadi@purdue.edu>
2016-04-05 18:32:06 -0400

    Buggy all pair alignment

--- run_allFamilies_sparse.sh
+++ run_allFamilies_sparse.sh
@@ -9,7 +9,7 @@ for instance_id in {1..1}; do
 	SeqSim=$INPUT_PATH/$instance/A-B.smat
 
 	rm output/*
-	./tri-match -t smat -H $H_net -G $G_net -S $SeqSim -Y 2 -a $alpha -b 0 --iter 3 -x seqsim
+	echo ./tri-match -t smat -H $H_net -G $G_net -S $SeqSim -Y 2 -a $alpha -b 0 --iter 3 -x seqsim
 	#fname=`ls output/ -1t | head -1`
 	#outpath=/home/shahin/Dropbox/Projects/HONEA/experiment/output/NAPA/sorted/Family_$instance_id/cTAME_alpha=0.5
 	#mkdir -p $outpath
