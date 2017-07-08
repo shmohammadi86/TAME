cae733d3bfc3f10b2197950dca3a2f4d50dd90f8
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2017-07-08 07:19:22 -0400

    run scripts has been updated

--- sparse_run.sh
+++ sparse_run.sh
@@ -1,2 +1,2 @@
-./tri-match -t smat -H input/BioGRID/yeast_net.smat -G input/BioGRID/human_net.smat -S input/BioGRID/human-yeast.smat -Y 2 -a 0.85 -b 0.1 --iter 3 -x seqsim --post_iter 0
+./tri-match -t tab -H input/BioGRID_full_yeast_net.tsv -G input/BioGRID_full_human_net.tsv -S input/BioGRID_full_human-yeast.evals -Y 2 -a 0.85 -b 0.1 --iter 3 -x seqsim --post_iter 3
 
